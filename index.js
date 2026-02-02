17627f296991702108c2ce075caa6759d39bd6e4
ferleon88 <fervid1986@gmail.com>
2026-02-01 21:33:01 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("✅ Bot WhatsApp Fervid activo");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
