5a694e8e80d406aa5203d47bc700ac59052ef8cd
ferleon88 <fervid1986@gmail.com>
2026-02-03 20:13:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import makeWASocket, {
 } from "@whiskeysockets/baileys";
 import express from "express";
 import qrcode from "qrcode-terminal";
+import qrcodeWeb from "qrcode";
+
 
 
 
@@ -12,6 +14,19 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("✅ Bot WhatsApp Fervid activo");
+});
+app.get("/qr", async (req, res) => {
+  if (!global.latestQR) return res.send("❌ Aún no hay QR. Espera 10 segundos y recarga.");
+
+  const img = await qrcodeWeb.toDataURL(global.latestQR);
+
+  res.send(`
+    <html>
+      <body style="display:flex;justify-content:center;align-items:center;height:100vh;background:#111;">
+        <img src="${img}" style="width:320px;height:320px;background:white;padding:12px;border-radius:12px" />
+      </body>
+    </html>
+  `);
 });
 
 const PORT = process.env.PORT || 8080;
